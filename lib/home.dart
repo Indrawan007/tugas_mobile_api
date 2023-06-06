8a9d26cc91b70d22d1e2c600dd864126c0318feb
Indrawan007 <phantom.el777@gmail.com>
2023-06-06 19:46:53 +0800

    Fix UI & Finishing

--- lib/home.dart
+++ lib/home.dart
@@ -8,7 +8,11 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("Get API"),
+        centerTitle: true,
+        elevation: 0,
+      ),
       body: FutureBuilder<List<Test>>(
         future: getTests(),
         builder: (context, snapshot) {
@@ -23,13 +27,56 @@ class MyHomePage extends StatelessWidget {
               itemCount: testList.length,
               itemBuilder: (context, index) {
                 Test testData = testList[index];
-                return Column(
-                  children: [
-                    Text("${testData.userId}"),
-                    Text("${testData.id}"),
-                    Text("${testData.title}"),
-                    Text("${testData.completed}"),
-                  ],
+                return Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Row(
+                        children: [
+                          Container(
+                            height: 40,
+                            width: 40,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(100),
+                                color: Colors.black),
+                            child: Align(
+                                alignment: Alignment.center,
+                                child: Text(
+                                  "${index + 1}",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                )),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Flexible(
+                            fit: FlexFit.loose,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("userId    : ${testData.userId}"),
+                                Text("id            : ${testData.id}"),
+                                Text(
+                                  "${testData.title}",
+                                  overflow: TextOverflow.visible,
+                                  softWrap: true,
+                                ),
+                                Text("completed : ${testData.completed}"),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 );
               },
             );
