a265bca350b73bbd4029bf13f8ae17fafaba1908
Indrawan007 <phantom.el777@gmail.com>
2023-06-06 12:27:29 +0800

    Create Model

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,21 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+import 'dart:convert';
 
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
 
-import 'package:tugas_mobile_api/main.dart';
+void main() async {
+  final response = await http.get(
+    Uri.parse('https://jsonplaceholder.typicode.com/todos/'),
+  );
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  final data = json.decode(response.body);
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+  // final Map<String, dynamic> data = json.decode(response.body);
+  print(data);
+  // if (response.statusCode == 200) {
+  //   final Map<String, dynamic> data = json.decode(response.body);
+  //   print(data);
+  //   return AllChapters.fromJson(data);
+  // } else {
+  //   throw Exception('Failed to fetch comic chapters');
+  // }
 }
